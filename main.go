886257432c6946f53b6133201227ce68c930d9e7
Stephen Mann <thanthese@gmail.com>
2015-11-06 14:57:49 -0600

    changed to go implementation

---
+++ main.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const weekdays = "umtwrfs"
+
+func main() {
+	today := flag.Bool("t", false, "Don't worry about inputs, just print today.")
+	plus := flag.Int("p", 0, "Days to add, on top of whatever else.")
+	flag.Parse()
+
+	if *today {
+		fmt.Println(prettyPrint(time.Now()))
+		return
+	}
+
+	bytes, _ := ioutil.ReadAll(os.Stdin)
+	s := string(bytes)
+	fmt.Printf(Bump(s, time.Now(), *plus))
+}
+
+// Main entry point for program logic. See readme for accepted syntax.
+func Bump(s string, today time.Time, plus int) string {
+	f := extractFields(s)
+	date, err := calcDate(f, today)
+	if err != nil {
+		return err.Error()
+	}
+	date = add(f.addSign, f.addAmount, f.addUnit, date)
+	date = date.AddDate(0, 0, plus)
+	if f.hasYMD() && f.hasNoAdd() && plus == 0 {
+		switch f.repeatType {
+		case "+":
+			date = add("+", f.repeatAmount, f.repeatUnit, today)
+		case ">":
+			date = add("+", f.repeatAmount, f.repeatUnit, date)
+		case "|":
+			date = addSpecial(f, date)
+		}
+	}
+	return prettyPrint(date) + calcSeparator(f) + f.description
+}
+
+// All useful information pulled out of the input string. In an ideal world
+// this stuff would be converted into more useful types, but strings have an
+// important property: they're easily nullable. Also, extracting this
+// information is hard enough already without having to worry about type
+// conversions, too.
+type fields struct {
+	year    string
+	month   string
+	day     string
+	weekday string
+
+	addSign   string
+	addAmount string
+	addUnit   string
+
+	description string
+
+	repeatType   string
+	repeatAmount string
+	repeatUnit   string
+
+	specialSign    string
+	specialWeekday string
+}
+
+func (f fields) hasYMD() bool {
+	return f.year != "" && f.month != "" && f.day != ""
+}
+
+func (f fields) hasNoDate() bool {
+	return f.year == "" && f.month == "" && f.day == "" && f.weekday == ""
+}
+
+func (f fields) hasNoAdd() bool {
+	return f.addSign == ""
+}
+
+// Parse the input string into something intelligible. Historically this has
+// been the most difficult party of the problem. Here's it's all handled at
+// once -- one big mess, completely separated from what we'll do with it later.
+func extractFields(s string) (f fields) {
+	find := func(reg, s string) []string { return regexp.MustCompile(reg).FindStringSubmatch(s) }
+
+	var dateStr string
+	if m := find(`^(\d{1,2})\.(\d{1,2})\.(\d{1,2})([mtwrfsu]?)\b`, s); m != nil {
+		dateStr, f.year, f.month, f.day, f.weekday = m[0], m[1], m[2], m[3], m[4]
+	} else if m := find(`^(\d{1,2})\.(\d{1,2})([mtwrfsu]?)\b`, s); m != nil {
+		dateStr, f.month, f.day, f.weekday = m[0], m[1], m[2], m[3]
+	} else if m := find(`^(\d{1,2})([mtwrfsu]?)\b`, s); m != nil {
+		dateStr, f.day, f.weekday = m[0], m[1], m[2]
+	} else if m := find(`^([mtwrfsu])\b`, s); m != nil {
+		dateStr, f.weekday = m[0], m[1]
+	}
+	r := strings.Replace(s, dateStr, "", 1)
+
+	var addStr string
+	if m := find(`^([+-])(\d*)([dwmy]?)`, r); m != nil {
+		addStr, f.addSign, f.addAmount, f.addUnit = m[0], m[1], m[2], m[3]
+	}
+	r = strings.Replace(r, addStr, "", 1)
+
+	f.description = r
+
+	if m := find(`( |\(|^)([+>])(\d*)([dwmy]?)( |\)|$)`, r); m != nil {
+		f.repeatType, f.repeatAmount, f.repeatUnit = m[2], m[3], m[4]
+	}
+	if m := find(`( |\(|^)(\|)([my])([+-])(\d)([umtwrfs])( |\)|$)`, r); m != nil {
+		f.repeatType, f.repeatUnit, f.specialSign, f.repeatAmount, f.specialWeekday = m[2], m[3], m[4], m[5], m[6]
+	}
+	return
+}
+
+func prettyPrint(t time.Time) string {
+	return fmt.Sprintf("%02d.%02d.%02d%c", t.Year()-2000, int(t.Month()), t.Day(), weekdays[t.Weekday()])
+}
+
+// Take what the user gave us and try to figure out what date they were talking
+// about.
+func calcDate(f fields, today time.Time) (time.Time, error) {
+	if f.hasNoDate() {
+		return today, nil
+	}
+	if f.hasYMD() {
+		y := atoi(f.year) + 2000
+		m := time.Month(atoi(f.month))
+		d := atoi(f.day)
+		return time.Date(y, m, d, 0, 0, 0, 0, time.UTC), nil
+	}
+	d := today
+	for tries := 0; tries < 365*10; tries++ {
+		d = d.AddDate(0, 0, 1)
+		if (f.month != "" && d.Month() != time.Month(atoi(f.month))) ||
+			(f.day != "" && d.Day() != atoi(f.day)) ||
+			(f.weekday != "" && d.Weekday() != time.Weekday(strings.Index(weekdays, f.weekday))) {
+			continue
+		}
+		return d, nil
+	}
+	return today, errors.New(fmt.Sprintf("ERROR: Weird format %s.%s%s for %s ", f.month, f.day, f.weekday, prettyPrint(d)))
+}
+
+// Add some amount to the given date. Handles the vagaries of strings and
+// defaults.
+func add(sign, amount, unit string, date time.Time) time.Time {
+	if sign == "" {
+		return date
+	}
+	if amount == "" {
+		amount = "1"
+	}
+	amt := atoi(sign + amount)
+	switch unit {
+	case "y":
+		return date.AddDate(amt, 0, 0)
+	case "m":
+		return date.AddDate(0, amt, 0)
+	case "w":
+		return date.AddDate(0, 0, amt*7)
+	default:
+		return date.AddDate(0, 0, amt)
+	}
+}
+
+// Add involving the special "|" syntax.
+func addSpecial(f fields, date time.Time) time.Time {
+
+	// move to the 1st of the appropriate month
+	date = time.Date(date.Year(), date.Month(), 1, 0, 0, 0, 0, time.UTC)
+	if f.repeatUnit == "m" {
+		date = date.AddDate(0, 1, 0)
+	} else {
+		date = date.AddDate(1, 0, 0)
+	}
+
+	// build up a list of candidate days in that month
+	days := []time.Time{}
+	for d := date; d.Month() == date.Month(); {
+		if d.Weekday() == time.Weekday(strings.Index(weekdays, f.specialWeekday)) {
+			days = append(days, d)
+		}
+		d = d.AddDate(0, 0, 1)
+	}
+
+	// return the right candidate
+	index := atoi(f.specialSign+f.repeatAmount) - 1
+	if index < 0 {
+		index += len(days) + 1
+	}
+	return days[index]
+}
+
+func calcSeparator(f fields) string {
+	if f.hasNoDate() && f.hasNoAdd() && f.description != "" {
+		return " "
+	}
+	return ""
+}
+
+// My regexes are guaranteed to provide valid ints, so we can do away with the
+// error checking.
+func atoi(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
---
+++ main_test.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestBump(t *testing.T) {
+	cases := []struct {
+		in   string
+		plus int
+		want string
+	}{
+		// dates only
+		{"10", 0, "15.09.10r"},
+		{"04", 0, "15.10.04u"},
+		{"4", 0, "15.10.04u"},
+		{"9.20", 0, "15.09.20u"},
+		{"10.5", 0, "15.10.05m"},
+		{"1.05", 0, "16.01.05t"},
+		{"1.05w", 0, "22.01.05w"},
+		{"15.10.05", 0, "15.10.05m"},
+		{"15.10.05m", 0, "15.10.05m"},
+		{"u", 0, "15.09.13u"},
+		{"w", 0, "15.09.09w"},
+		{"s", 0, "15.09.12s"},
+		{"15.09.06u", 0, "15.09.06u"},
+		{"15.09.05s", 0, "15.09.05s"},
+
+		// fixed adds only
+		{"+", 0, "15.09.07m"},
+		{"+1", 0, "15.09.07m"},
+		{"+d", 0, "15.09.07m"},
+		{"+1d", 0, "15.09.07m"},
+		{"+14d", 0, "15.09.20u"},
+		{"+28d", 0, "15.10.04u"},
+		{"+w", 0, "15.09.13u"},
+		{"+1w", 0, "15.09.13u"},
+		{"+4w", 0, "15.10.04u"},
+		{"+m", 0, "15.10.06t"},
+		{"+1m", 0, "15.10.06t"},
+		{"+2m", 0, "15.11.06f"},
+		{"+y", 0, "16.09.06t"},
+		{"+1y", 0, "16.09.06t"},
+
+		// dates with adds
+		{"4+2", 0, "15.10.06t"},
+		{"10.4+2", 0, "15.10.06t"},
+		{"15.10.4+2", 0, "15.10.06t"},
+		{"15.10.4u+2d", 0, "15.10.06t"},
+
+		// no-repetition descriptions
+		{"", 0, "15.09.06u"},
+		{"test", 0, "15.09.06u test"},
+		{" test ", 0, "15.09.06u  test "},
+
+		// dates, fixed adds, and basic descriptions
+		{"10 test", 0, "15.09.10r test"},
+		{"+ test", 0, "15.09.07m test"},
+		{"t test", 0, "15.09.08t test"},
+		{"4+2 test", 0, "15.10.06t test"},
+		{"4+2  test ", 0, "15.10.06t  test "},
+		{"4+2  longer test ", 0, "15.10.06t  longer test "},
+
+		// descriptions with adds
+		{" +4d test", 0, "15.09.06u  +4d test"},
+		{"15.09.04f  +4d test", 0, "15.09.10r  +4d test"},
+		{"15.09.04f test +4d", 0, "15.09.10r test +4d"},
+		{"15.09.04f test +4d ", 0, "15.09.10r test +4d "},
+		{"15.09.04f test +5d +3d", 0, "15.09.11f test +5d +3d"},
+
+		// descriptions with jumps
+		{"test >4d", 0, "15.09.06u test >4d"},
+		{"15.11.04w >1d", 0, "15.11.05r >1d"},
+		{"15.03.04w >1d", 0, "15.03.05r >1d"},
+		{"15.03.04w >w", 0, "15.03.11w >w"},
+
+		// descriptions with byWeeks
+		{"15.09.06u |m+1m", 0, "15.10.05m |m+1m"},
+		{"15.09.06u |m+2m", 0, "15.10.12m |m+2m"},
+		{"15.09.06u |m-1m", 0, "15.10.26m |m-1m"},
+		{"15.09.06u |m-2m", 0, "15.10.19m |m-2m"},
+		{"15.09.06u |y+1m", 0, "16.09.05m |y+1m"},
+		{"15.09.06u |y-2m", 0, "16.09.19m |y-2m"},
+
+		// all together now
+		{"4+2w +2d test", 0, "15.10.18u +2d test"},
+
+		// plus
+		{"test", 1, "15.09.07m test"},
+		{"test", 2, "15.09.08t test"},
+		{"test", -1, "15.09.05s test"},
+		{"4+2w test +4d", 2, "15.10.20t test +4d"},
+
+		// accept ()
+		{"15.09.09w (+2d) test", 0, "15.09.08t (+2d) test"},
+		{"15.09.12s test a (>4d) b", 0, "15.09.16w test a (>4d) b"},
+		{"15.09.06u test (|m+1m)", 0, "15.10.05m test (|m+1m)"},
+
+		// weird inputs
+		{"15.09.09w test+2dtest test", 0, "15.09.09w test+2dtest test"},
+		{"15.09.09w +2dtest test", 0, "15.09.09w +2dtest test"},
+		{"99.99", 0, "ERROR"},
+	}
+
+	today := time.Date(2015, time.September, 6, 0, 0, 0, 0, time.UTC) // a sunday
+	for _, c := range cases {
+		got := Bump(c.in, today, c.plus)
+		if strings.Contains(c.want, "ERROR") {
+			if strings.Contains(got, "ERROR") {
+				continue
+			}
+			fmt.Println("really wanted to see an error", c.want, got)
+			t.Errorf("Was expecting an error on Bump(%q, %v, %v), but got %q instead.", prettyPrint(today), c.plus, c.in, got)
+		} else if got != c.want {
+			t.Errorf("Bump(%q, %v, %v) == %q, want %q", c.in, prettyPrint(today), c.plus, got, c.want)
+		}
+	}
+}
